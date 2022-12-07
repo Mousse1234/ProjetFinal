0bd75539f159017672e8a4981a7abee5a652c411
Roselyne Martin <roselyne.martin.01@edu.cegeptr.qc.ca>
2022-12-07 09:55:52 -0500

    trajets

--- ProjetFinale/ProjetFinale/TrajetEnCours.xaml.cs
+++ ProjetFinale/ProjetFinale/TrajetEnCours.xaml.cs
@@ -23,9 +23,36 @@ namespace ProjetFinale
     /// </summary>
     public sealed partial class TrajetEnCours : Page
     {
+        string[] strVehicules = { "VUS", "Berline" };
+
         public TrajetEnCours()
         {
             this.InitializeComponent();
+            cbTypeVehicule.ItemsSource = strVehicules;
+            cbTypeVehicule.SelectedIndex = 0;
+        }
+
+        private void Valider_Click(object sender, RoutedEventArgs e)
+        {
+            if(cbTypeVehicule.SelectedIndex == 0)
+            {
+                tbDate.Text = "maintenant";
+                tbHdepart.Text = "12h00pm";
+                tbHarrivee.Text = "18h30pm";
+                tbnbPlace.Text = "Le nombre de place disponible est de 5 passagers";
+                tbPrix.Text = "Le prix de chaque billets est de 15$";
+                Resultat.Visibility = Visibility.Visible;
+            }
+            else if(cbTypeVehicule.SelectedIndex == 1)
+            {
+                //tbDate.Text = DateTrajet;
+                tbHdepart.Text = "12h00pm";
+                tbHarrivee.Text = "18h30pm";
+                tbnbPlace.Text = "Le nombre de place disponible est de 3 passagers";
+                tbPrix.Text = "Le prix de chaque billets est de 10$";
+                Resultat.Visibility = Visibility.Visible;
+            }
+            
         }
     }
 }
---
+++ ProjetFinale/ProjetFinale/Trajets.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetFinale
+{
+    internal class Trajets
+    {
+        string dateTrajet;
+        string hdepart;
+        string harrivee;
+        string vehicules;
+        int nbPlace;
+        int prixPlace;
+        
+        public string DateTrajet { get => dateTrajet; set => dateTrajet = value; }
+        public string Hdepart { get => hdepart; set => hdepart = value; }
+        public string Harrivee { get => harrivee; set => harrivee = value; }
+        public string Vehicules { get => vehicules; set => vehicules = value; }
+        public int NbPlace { get => nbPlace; set => nbPlace = value; }
+        public int PrixPlace { get => prixPlace; set => prixPlace = value; }
+
+        public override string ToString()
+        {
+            return  "Date du trajet" + DateTrajet + "Heure départ" + Hdepart +  "heure arrivee" + Harrivee +
+                    "type de véhicule" + Vehicules + "nb place" + NbPlace + " prix de place" + PrixPlace;
+        }
+
+    }
+}
