20111f8b9d3631f69f841d3150b87dd1d4d6f975
Mathieu Mongrain <mathieu.mongrain.03@edu.cegeptr.qc.ca>
2022-12-13 20:00:13 -0500

    m

--- ProjetFinale/ProjetFinale/GestionBD.cs
+++ ProjetFinale/ProjetFinale/GestionBD.cs
@@ -7,6 +7,8 @@ using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
 
 namespace ProjetFinale
 {
